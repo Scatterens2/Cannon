3b7d383bcb5e47f52307f2abf8cd19b37c7deb60
Scatterens2 <88872461+Scatterens2@users.noreply.github.com>
2021-08-14 11:46:10 +0200

    Working GUIMarineBuyMenu buttons

--- lua/Cannon/GUIMarineBuyMenu.lua
+++ lua/Cannon/GUIMarineBuyMenu.lua
@@ -1,4 +1,5 @@
 GUIMarineBuyMenu.kButtonGroupFrame_Labeled_x1     = PrecacheAsset("ui/buymenu_marine/button_group_frame_labelled_x2.dds")
+GUIMarineBuyMenu.kButtonsTexture                  = PrecacheAsset("ui/buymenu_marine/buttons_16_slots.dds")
 
 local kExtraWeaponGroupButtonPositions = 
 {	
@@ -11,32 +12,22 @@ local kExtraWeaponGroupButtonPositions =
 
 
 local kTechIdInfo = debug.getupvaluex(GUIMarineBuyMenu._GetButtonPixelCoordinatesForTechID, "kTechIdInfo")
-Print("%s", ToString(kTechIdInfo))
-table.insert(kTechIdInfo, {kTechId.Cannon = {	ButtonTextureIndex = 15,
-												BigPictureIndex = 15, 
-												Description = "BLABLABLA",    
-												Stats = {   LifeFormDamage = 0.2,
-															StructureDamage = 0.8,
-															Range = 0.6,	
-														}
-							})
-Print("%s", ToString(kTechIdInfo))
-kTechIdInfo[kTechId.Cannon].ButtonTextureIndex
-
---table.insert(kTechIdInfo , {[kTechId.Cannon].ButtonTextureIndex = 14} )
-
-
---local _GetButtonPixelCoordinatesForTechID = debug.getupvaluex(GUIMarineBuyMenu._CreateButton, "_GetButtonPixelCoordinatesForTechID")
 local kTechIdInfo = debug.getupvaluex(GUIMarineBuyMenu._GetPigPicturePixelCoordinatesForTechID, "kTechIdInfo")
---kTechIdInfo[kTechId.Cannon].BigPictureIndex =12
-
-
 local kTechIdInfo = debug.getupvaluex(GUIMarineBuyMenu._SetDetailsSectionTechId, "kTechIdInfo")
---kTechIdInfo[kTechId.Cannon].Stats = {
---						LifeFormDamage = 1,
---						StructureDamage = 1,
---						Range = 1,
---					}
+
+table.insert(kTechIdInfo,	
+	kTechId.Cannon, 
+	{	
+		ButtonTextureIndex = 15,
+		BigPictureIndex = 15, 
+		Description = "BLABLABLA",    
+		Stats = {   LifeFormDamage = 0.8,
+					StructureDamage = 0.5,
+					Range = 1,	
+				}
+	}
+ )
+
 
 function GUIMarineBuyMenu:CreateExtendedArmoryUI()
 	
